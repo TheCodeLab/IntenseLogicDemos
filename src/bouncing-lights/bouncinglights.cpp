76ac9b99d25a50e4dff1451232c38391d5ad3a06
Tiffany Bennett <tiffany@stormbit.net>
2015-10-16 19:50:01 -0400

    Make bouncing lights stop crashing

--- src/bouncing-lights/bouncinglights.cpp
+++ src/bouncing-lights/bouncinglights.cpp
@@ -10,6 +10,8 @@
 #include <ctime>
 #include <math.h>
 
+#include <experimental/optional>
+
 #include "tgl/tgl.h"
 #include "debugdraw.hpp"
 #include "bulletspace.hpp"
@@ -42,47 +44,159 @@ float rand_float(unsigned *seedp)
     return (float)rand_r(seedp) / RAND_MAX;
 }
 
-struct Spheres {
+struct Scene : public Drawable {
+    Scene(BulletSpace &space)
+        : space(space) {}
+
+    BulletSpace &space;
     vector<BulletSpace::BodyID> bodies;
     vector<il_vec3> colors;
     vector<ilG_light> lights;
-};
+    experimental::optional<BulletSpace::BodyID> heightmap_body;
+    ilG_heightmap heightmap;
+    BallRenderer ball;
+    btSphereShape sphere_shape = btSphereShape(1);
+    btStaticPlaneShape ground_shape[4] = {
+        btStaticPlaneShape(btVector3( 1, 0,  0), 1),
+        btStaticPlaneShape(btVector3(-1, 0,  0), 1),
+        btStaticPlaneShape(btVector3( 0, 0,  1), 1),
+        btStaticPlaneShape(btVector3( 0, 0, -1), 1)
+    };
+    btDefaultMotionState ground_motion_state[4], heightmap_motion_state;
+    experimental::optional<btHeightfieldTerrainShape> heightmap_shape;
 
-Spheres add_objects(BulletSpace &bs, btCollisionShape *shape, size_t count, unsigned *seedp)
-{
-    Spheres spheres;
-    for (size_t i = 0; i < count; i++) {
-        // Physics body
-        btVector3 position
-            (rand_r(seedp) % 128,
-             rand_r(seedp) % 32 + 50,
-             rand_r(seedp) % 128);
-        auto state = new btDefaultMotionState(btTransform(btQuaternion(0,0,0,1), position));
-        float mass = 1.f;
-        btVector3 inertia(0,0,0);
-        shape->calculateLocalInertia(mass, inertia);
-        btRigidBody::btRigidBodyConstructionInfo ballRigidBodyCI(mass, state, shape, inertia);
-        auto id = bs.add(ballRigidBodyCI);
-        bs.getBody(id).setRestitution(1.0);
+    void draw(Graphics &graphics) override {
+        (void)graphics;
+        il_mat hmvp, himt;
+        space.objmats(&hmvp, &*heightmap_body, ILG_MVP, 1);
+        space.objmats(&himt, &*heightmap_body, ILG_IMT, 1);
+        ilG_heightmap_draw(&heightmap, hmvp, himt);
 
-        // Color
-        float brightness = rand_float(seedp) + 1;
-        auto col = il_vec3_new(
-            rand_float(seedp) * brightness,
-            rand_float(seedp) * brightness,
-            rand_float(seedp) * brightness);
-
-        // Light
-        ilG_light light;
-        light.color = col;
-        light.radius = brightness * 10;
-
-        spheres.bodies.push_back(id);
-        spheres.colors.push_back(col);
-        spheres.lights.push_back(light);
+        vector<il_mat> mvp, imt;
+        mvp.resize(bodies.size());
+        imt.resize(bodies.size());
+        space.objmats(mvp.data(), bodies.data(), ILG_MVP, bodies.size());
+        space.objmats(imt.data(), bodies.data(), ILG_IMT, bodies.size());
+        ball.draw(mvp.data(), imt.data(), colors.data(), bodies.size());
     }
-    return spheres;
-}
+
+    bool build(ilG_renderman *rm) {
+        // Arena walls
+        ///////////////
+        vector<btRigidBody> ground_body;
+        ground_body.reserve(4);
+        btVector3 positions[4] = {
+            btVector3(0, 0, 0),
+            btVector3(arenaWidth, 0, 0),
+            btVector3(0, 0, 0),
+            btVector3(0, 0, arenaWidth)
+        };
+        for (unsigned i = 0; i < 4; i++) {
+            ground_motion_state[i] = btDefaultMotionState(btTransform(btQuaternion(0,0,0,1),positions[i]));
+            btRigidBody::btRigidBodyConstructionInfo groundRigidBodyCI
+                (0,
+                 &ground_motion_state[i],
+                 &ground_shape[i],
+                 btVector3(0,0,0) );
+            auto id = space.add(groundRigidBodyCI);
+            space.getBody(id).setRestitution(0.5);
+        }
+
+        // Image
+        ///////////////////////
+        ilA_img hm;
+        ilA_imgerr res = ilA_img_loadfile(&hm, &demo_fs, "arena-heightmap.png");
+        if (res) {
+            il_error("Failed to load heightmap: %s", ilA_img_strerror(res));
+            return false;
+        }
+        // Physics
+        /////////////////////
+        const unsigned height = 50;
+        heightmap_shape = btHeightfieldTerrainShape(hm.width, hm.height, hm.data, height/255.f, 0,
+                                                    height, 1, PHY_UCHAR, false);
+        heightmap_shape->setLocalScaling(btVector3(arenaWidth/hm.width, 1, arenaWidth/hm.height));
+        btTransform trans = btTransform(btQuaternion(0,0,0,1),
+                                        btVector3(arenaWidth/2, height/2, arenaWidth/2));
+        heightmap_motion_state = trans;
+        btRigidBody::btRigidBodyConstructionInfo groundRigidBodyCI
+            (0, &heightmap_motion_state, &*heightmap_shape, btVector3(0,0,0));
+        heightmap_body = space.add(groundRigidBodyCI);
+        space.getBody(*heightmap_body).setRestitution(1.0);
+        space.setBodyScale(*heightmap_body, il_vec3_new(128, 50, 128));
+        // Rendering
+        ///////////////////////
+        ilA_img norm;
+        res = ilA_img_height_to_normal(&norm, &hm);
+        if (res) {
+            il_error("Failed to create normal map: %s", ilA_img_strerror(res));
+            return false;
+        }
+        ilG_tex colortex, heighttex, normaltex;
+        res = ilG_tex_loadfile(&colortex, &demo_fs, "terrain.png");
+        if (res) {
+            il_error("Failed to load heightmap texture: %s", ilA_img_strerror(res));
+            return false;
+        }
+        ilA_img hmc;
+        res = ilA_img_copy(&hmc, &hm);
+        if (res) {
+            il_error("Failed to copy image: %s", ilA_img_strerror(res));
+            return false;
+        }
+        ilG_tex_loadimage(&heighttex, hmc);
+        ilG_tex_loadimage(&normaltex, norm);
+        char *error;
+        if (!ilG_heightmap_build(&this->heightmap, rm, hm.width, hm.height,
+                                 heighttex, normaltex, colortex, &error)) {
+            il_error("heightmap: %s", error);
+            free(error);
+            return false;
+        }
+
+        if (!ball.build(rm, &error)) {
+            il_error("ball: %s", error);
+            free(error);
+            return false;
+        }
+
+        return true;
+    }
+
+    void populate(size_t count) {
+        unsigned seed;
+        for (size_t i = 0; i < count; i++) {
+            // Physics body
+            btVector3 position
+                (rand_r(&seed) % 128,
+                 rand_r(&seed) % 32 + 50,
+                 rand_r(&seed) % 128);
+            auto state = new btDefaultMotionState(btTransform(btQuaternion(0,0,0,1), position));
+            float mass = 1.f;
+            btVector3 inertia(0,0,0);
+            sphere_shape.calculateLocalInertia(mass, inertia);
+            btRigidBody::btRigidBodyConstructionInfo ballRigidBodyCI(mass, state, &sphere_shape, inertia);
+            auto id = space.add(ballRigidBodyCI);
+            space.getBody(id).setRestitution(1.0);
+
+            // Color
+            float brightness = rand_float(&seed) + 1;
+            auto col = il_vec3_new(
+                rand_float(&seed) * brightness,
+                rand_float(&seed) * brightness,
+                rand_float(&seed) * brightness);
+
+            // Light
+            ilG_light light;
+            light.color = col;
+            light.radius = brightness * 10;
+
+            bodies.push_back(id);
+            colors.push_back(col);
+            lights.push_back(light);
+        }
+    }
+};
 
 int main(int argc, char **argv)
 {
@@ -123,80 +237,13 @@ int main(int argc, char **argv)
 
     // Setup invisible arena walls
     ///////////////////////////////
-    btStaticPlaneShape groundShape[4] = {
-        btStaticPlaneShape(btVector3( 1, 0,  0), 1),
-        btStaticPlaneShape(btVector3(-1, 0,  0), 1),
-        btStaticPlaneShape(btVector3( 0, 0,  1), 1),
-        btStaticPlaneShape(btVector3( 0, 0, -1), 1)
-    };
-    btDefaultMotionState groundMotionState[4];
-    vector<btRigidBody> groundBody;
-    btVector3 positions[4] = {
-        btVector3(0, 0, 0),
-        btVector3(arenaWidth, 0, 0),
-        btVector3(0, 0, 0),
-        btVector3(0, 0, arenaWidth)
-    };
-    for (unsigned i = 0; i < 4; i++) {
-        groundMotionState[i] = btDefaultMotionState(btTransform(btQuaternion(0,0,0,1),positions[i]));
-        btRigidBody::btRigidBodyConstructionInfo groundRigidBodyCI
-            (0,
-             &groundMotionState[i],
-             &groundShape[i],
-             btVector3(0,0,0) );
-        groundBody.emplace_back(groundRigidBodyCI);
-        btRigidBody &groundRigidBody = groundBody.back();
-        groundRigidBody.setRestitution(0.5);
-        world.world.addRigidBody(&groundRigidBody);
-    }
 
-    // Create heightmap physics and render stuff
-    /////////////////////////////////////////////
-    ilA_img hm;
-    ilA_imgerr res = ilA_img_loadfile(&hm, &demo_fs, "arena-heightmap.png");
-    if (res) {
-        il_error("Failed to load heightmap: %s", ilA_img_strerror(res));
+    Scene scene(world);
+    if (!scene.build(graphics.rm)) {
         return 1;
     }
-    const unsigned height = 50;
-    btHeightfieldTerrainShape heightmap_shape
-        (hm.width, hm.height, hm.data, height/255.f, 0, height, 1, PHY_UCHAR, false);
-    heightmap_shape.setLocalScaling(btVector3(arenaWidth/hm.width, 1, arenaWidth/hm.height));
-    btTransform trans = btTransform(btQuaternion(0,0,0,1),
-                                    btVector3(arenaWidth/2, height/2, arenaWidth/2));
-    btDefaultMotionState heightmap_state(trans);
-    btRigidBody::btRigidBodyConstructionInfo groundRigidBodyCI
-        (0, &heightmap_state, &heightmap_shape, btVector3(0,0,0));
-    auto ground_id = world.add(groundRigidBodyCI);
-    world.getBody(ground_id).setRestitution(1.0);
-    world.setBodyScale(ground_id, il_vec3_new(128, 50, 128));
-    ilA_img norm;
-    res = ilA_img_height_to_normal(&norm, &hm);
-    if (res) {
-        il_error("Failed to create normal map: %s", ilA_img_strerror(res));
-        return 1;
-    }
-    ilG_tex colortex, heighttex, normaltex;
-    res = ilG_tex_loadfile(&colortex, &demo_fs, "terrain.png");
-    if (res) {
-        il_error("Failed to load heightmap texture: %s", ilA_img_strerror(res));
-        return 1;
-    }
-    ilA_img hmc;
-    res = ilA_img_copy(&hmc, &hm);
-    if (res) {
-        il_error("Failed to copy image: %s", ilA_img_strerror(res));
-        return 1;
-    }
-    ilG_tex_loadimage(&heighttex, hmc);
-    ilG_tex_loadimage(&normaltex, norm);
-
-    // Create ball renderer and common bullet stuff
-    ////////////////////////////////////////////////
-    btSphereShape ball_shape(1);
-    BallRenderer ball;
-    unsigned seed = time(NULL);
-    add_objects(world, &ball_shape, 100, &seed);
+    scene.populate(100);
+    graphics.drawables.push_back(&scene);
 
     float yaw = 0, pitch = 0;
     il_quat rot = il_quat_new(0,0,0,1);
@@ -235,7 +282,7 @@ int main(int argc, char **argv)
             playerwalk = il_vec3_rotate(playerwalk, rot);
             player.setWalkDirection(btVector3(playerwalk.x, playerwalk.y, playerwalk.z));
         }
-        //world.step(1/60.f);
+        world.step(1/60.f);
         graphics.draw(state);
     }
 }
